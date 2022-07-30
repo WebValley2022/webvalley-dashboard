7f75483a36b43dbe6f68d8b6e27f2134d887885f
Davide-FF <davide.frena@protonmail.com>
2022-07-30 12:25:55 +0200

    Added sidebar

--- plotly-app/app.py
+++ plotly-app/app.py
@@ -1,3 +1,4 @@
+import dash
 from dash import Dash, callback, html, dcc, Input, Output
 import dash_bootstrap_components as dbc
 import plotly.express as px
@@ -7,11 +8,37 @@ app = Dash(__name__,
            use_pages=True,
            external_stylesheets=[dbc.themes.BOOTSTRAP])
 
-df = pd.read_csv("../data/merged_APPA_data.csv")
-df.Data = pd.to_datetime(df.Data)
+SIDEBAR_STYLE = {
+    "position": "fixed",
+    "top": 0,
+    "left": 0,
+    "bottom": 0,
+    "width": "16rem",
+    "padding": "2rem 1rem",
+    "background-color": "#f8f9fa",
+}
+
+
+sidebar = html.Div([
+    html.H2("Sidebar", className="display-4"),
+    dbc.Nav([
+        dbc.NavLink("Section 1", href="/section-1", active="exact"),
+        dbc.NavLink("Section 2", href="/section-2", active="exact"),
+        dbc.NavLink("Section 3", href="/section-3", active="exact")
+    ],
+        vertical=True,
+        pills=True,
+        style=SIDEBAR_STYLE,
+    ),
+])
+
+content = html.Div(id="page-content")
 
 app.layout = html.Div([
-    html.H1('Section1'),
+    content,
+    sidebar,
+
+    dash.page_container
 ])
 
 
---
+++ plotly-app/pages/section-1.py
@@ -0,0 +1,10 @@
+import dash
+from dash import html, dcc, Input, Output
+import plotly.express as px
+import pandas as pd
+
+dash.register_page(__name__)
+
+layout = html.Div([
+    html.H1('Section 1'),
+])
