fcec765f37f46254ad7edc4564b926ae56dc49bd
Davide-FF <davide.frena@protonmail.com>
2022-07-30 12:09:33 +0200

    Fixed plotly app

--- plotly-app/app.py
+++ plotly-app/app.py
@@ -11,11 +11,7 @@ df = pd.read_csv("../data/merged_APPA_data.csv")
 df.Data = pd.to_datetime(df.Data)
 
 app.layout = html.Div([
-    html.H1('Dashboard'),
-    html.Label("Seleziona la stazione"),
-    dcc.Dropdown(df.Stazione.unique(), id="input-station"),
-    dcc.Dropdown(df.Inquinante.unique(), id='pollutant'),
-    dcc.Graph(id="pollutants-graph")
+    html.H1('Section1'),
 ])
 
 
--- plotly-app/pages/Section1.py
+++ plotly-app/pages/Section1.py
@@ -4,3 +4,7 @@ import plotly.express as px
 import pandas as pd
 
 dash.register_page(__name__)
+
+layout = html.Div([
+    html.H1('Section 2'),
+])
