053107ed11bb782b4af1801753f1bd86637c56f0
Davide-FF <davide.frena@protonmail.com>
2022-07-30 12:07:00 +0200

    Corrected app.py

--- plotly-app/app.py
+++ plotly-app/app.py
@@ -10,43 +10,13 @@ app = Dash(__name__,
 df = pd.read_csv("../data/merged_APPA_data.csv")
 df.Data = pd.to_datetime(df.Data)
 
-
-def filter_df(df, station, pollutant):
-    mask = (df.Inquinante == pollutant) & (df.Stazione == station)
-    filtered = df[mask]
-    return filtered
-
-
-def resample_df(df, resample_rule):
-    df_mean = df.resample(resample_rule, on='Data').mean()
-    df_mean = df_mean.reset_index()
-    return df_mean
-
-
 app.layout = html.Div([
-    html.H1(str(df.dtypes)),
+    html.H1('Dashboard'),
     html.Label("Seleziona la stazione"),
     dcc.Dropdown(df.Stazione.unique(), id="input-station"),
     dcc.Dropdown(df.Inquinante.unique(), id='pollutant'),
     dcc.Graph(id="pollutants-graph")
 ])
-
-
-@callback(
-    Output('pollutants-graph', 'figure'),
-    Input('input-station', 'value'),
-    Input('pollutant', 'value'))
-def update_graph(input_station, pollutant):
-    filtered_df = filter_df(df, input_station, pollutant)
-    compressed_df = resample_df(filtered_df, 'W')
-
-    if compressed_df.shape[0] > 0:
-        fig = px.line(
-            compressed_df,
-            y='Valore',
-        )
-
-    return fig
 
 
 if __name__ == "__main__":
