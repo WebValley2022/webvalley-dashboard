e574c8731b56f3bf7609d906e9d16b26cddde46e
Davide <dmr475@gmail.com>
2022-08-01 12:54:32 +0200

    add comparison graph function

--- plotly/graphs_functions.py
+++ plotly/graphs_functions.py
@@ -1,3 +1,6 @@
+#####################
+# NEEDED DATAFRAMES #
+#####################
 
 ###########################################
 # MAIN DAILY GRAPH OF PERCENTAGE TO LIMIT #
@@ -27,7 +30,6 @@ def update_daily_graph(pollutant):
         data_before,
         x = "date",
         y = "value",
-        color = "pollutant",
         markers = True
     )
 
@@ -35,7 +37,6 @@ def update_daily_graph(pollutant):
         data_previs,
         x = "date",
         y = "value",
-        color = "pollutant",
         markers = True,
         line_dash_sequence = ["dash"]
     )
@@ -56,6 +57,57 @@ def update_daily_graph(pollutant):
         line_color = "green",
         line_dash = "dash"
     )
+    return fig
+
+####################
+# SECTION 2 GRAPHS #
+####################
+
+# TODO: add callback
+def update_comparison_graph(
+    station: str,
+    pollutant: str,
+    time_span: int,
+    compare_APPA: bool
+):
+    """
+    Updates the graph representing the comparison between
+    APPA data and model prediction
+
+    Args:
+        station (str): station name
+        pollutant (str): pollutant name
+        time_span (int): 0: hour; 1: day; 2: week; 3: month; 4: year
+        compare_APPA (bool): wether to add a line of APPA's data or not
+    """
+    fig = go.Figure()
+    prevision_data = data_before[
+        (data_before["station"] == station) &
+        (data_before["pollutant"] == pollutant)
+    ]
+
+    fig.add_trace(
+        go.Scatter(
+            x = prevision_data["date"],
+            y = prevision_data["value"],
+            mode = "lines+markers"
+        )
+    )
+
+    if compare_APPA:
+        appa_data = data_before[
+            (data_before["station"] == station) &
+            (data_before["pollutant"] == "PM2.5")
+        ]
+
+        fig.add_trace(
+            go.Scatter(
+                x = appa_data["date"],
+                y = appa_data["value"],
+                mode = "lines+markers"
+            )
+        )
+
     return fig
 
 
