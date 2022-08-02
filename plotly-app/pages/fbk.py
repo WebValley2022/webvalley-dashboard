39c0ab0b18a4df90956e862c6a37f9b41d58e8dc
Davide-FF <davide.frena@protonmail.com>
2022-08-02 12:59:22 +0200

    Added graphs in appa.py

--- plotly-app/pages/fbk.py
+++ plotly-app/pages/fbk.py
@@ -1,5 +1,5 @@
 import dash
-from dash import html, dcc, Input, Output
+from dash import html, dcc, Input, Output, callback
 import plotly.express as px
 import dash_bootstrap_components as dbc
 import pandas as pd
@@ -8,13 +8,13 @@ dash.register_page(__name__)
 
 title = html.Div("FBK Fitted Data", className="header-title")
 
-dropdown = dbc.DropdownMenu(
-    label="Select location",
-    children=[
-        dbc.DropdownMenuItem("Trento - S. Chiara"),
-        dbc.DropdownMenuItem("Trento - Via Bolzano")
-    ],
-    color="secondary"
+df = pd.read_csv("../data/merged_APPA_data.csv")
+df = df[df.Valore != 'n.d.']
+df.Data = pd.to_datetime(df.Data)
+stations = df.Stazione.unique()
+
+dropdown = dcc.Dropdown(
+    stations, id='selected-station', className="dropdown", value=stations[0]
 )
 
 download_btn = dbc.Button(
@@ -23,10 +23,31 @@ download_btn = dbc.Button(
     class_name="download-btn"
 )
 
+gas_btns = html.Div(id="pollutants", className="radio-group")
+
 header = html.Div(
-    [title, dropdown, download_btn],
+    [title, dropdown, download_btn, gas_btns],
     className="section-header"
 )
+
+
+@callback(Output("pollutants", "children"), Input("selected-station", "value"))
+def get_pollutants(selected_station):
+    filtered_df = df[df.Stazione == selected_station]
+    pollutants = filtered_df.Inquinante.unique()
+    pollutants_dict = [{"label": pollutant, "value": pollutant}
+                       for pollutant in pollutants]
+
+    return dbc.RadioItems(
+        id="selected-pollutant",
+        class_name="btn-group",
+        input_class_name="btn-check",
+        label_class_name="btn btn-outline-primary",
+        label_checked_class_name="active",
+        options=pollutants_dict,
+        value=pollutants_dict[0]
+    )
+
 
 layout = html.Div(
     header,
