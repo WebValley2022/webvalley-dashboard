79bf8089225ceb25ee0cdeb94ad2849ca0451671
Davide <dmr475@gmail.com>
2022-08-02 12:38:32 +0200

    refactor update_daily_mean function, add comments, add util function 'get_percentage'

--- plotly/graphs_functions.py
+++ plotly/graphs_functions.py
@@ -1,3 +1,8 @@
+import plotly
+import plotly.graph_objs as go
+import datetime as dt
+import pandas as pd
+
 #####################
 #  UTILS FUNCTIONS  #
 #####################
@@ -11,67 +16,116 @@ pollutant_limit = {
     "PM2.5": 25               # daily mean missing, used year, incorrect, approximated
 }
 
-def get_mean(dataframe, time_span, station, pollutant):
-        mean_temp = dataframe[
-            (dataframe["station"] == station) &
-            (dataframe["pollutant"] == pollutant)
-        ].groupby(
-            by = pd.Grouper(
-                key  = "date",
-                freq = time_span
-            )
-        ).mean()
-        mean_temp.insert(1, "pollutant", pollutant)
-        mean_temp.insert(1, "station", station)
-        mean_temp.reset_index(inplace = True)
+def get_mean(dataframe: pd.DataFrame, time_span: str, station: str, pollutant: str) -> pd.DataFrame:
+    """
+    Gets the mean of a given time span from the given station
+    and pollutant in the given dataframe
 
-        return mean_temp
+    Args:
+        dataframe (pd.DataFrame): the input dataframe to be processed
+        time_span (str): values can be 'D': day, 'Y': year, 'H': hour
+        station (str): the station where to get the data
+        pollutant (str): the desired pollutant
+
+    Returns:
+        pd.DataFrame: the dataframe with the mean values
+    """
+    mean_temp = dataframe[
+        (dataframe["station"] == station) &
+        (dataframe["pollutant"] == pollutant)
+    ].groupby(
+        by = pd.Grouper(
+            key  = "date",
+            freq = time_span
+        )
+    ).mean()
+    mean_temp.insert(1, "pollutant", pollutant)
+    mean_temp.insert(1, "station", station)
+    mean_temp.reset_index(inplace = True)
+    
+    return mean_temp
+
+def get_percentage(dataframe: pd.DataFrame) -> pd.DataFrame:
+    """
+    Returns the percentage of the value of each pollutant
+    of the maximum legal level
+
+    Args:
+        dataframe (pd.DataFrame): the input dataframe
+
+    Returns:
+        pd.DataFrame: the processed dataframe
+    """
+
+    percentage_df = dataframe.copy()
+
+    val = percentage_df["value"]
+    pol = percentage_df["pollutant"]
+    for i in range(len(percentage_df)):
+        percentage_df.loc[i, "value"] = val[i]/pollutant_limit[pol[i]]
+
+    return percentage_df
 
 ###########################################
 # MAIN DAILY GRAPH OF PERCENTAGE TO LIMIT #
 ###########################################
-@app.callback(
-    Output('daily-percentage-graph', 'figure'),
-    Input('radio-pollutant-selector', 'value')
-)
+
 # VARIABLES NEEDED:
-#   - station_data_percentage: dataframe with percentage of pollutant value from the maximum legal level
-#   - SELECTED_STATION: the station that will be displayed
-def update_daily_graph(pollutant):
-    # date used to separate data
-    today = dt.date(2022, 7, 10)
+#   - past: dataframe with past pollutant records
+#   - prevision: dataframe with prevision data (will be processed into prevision_data)
+def update_daily_graph(station: str,  pollutant: str, time_span: str) -> plotly.graph_objs.Figure:
+    """
+    Updates the graph representing the daily trend
+    of the given pollutant in the given station
 
-    # condition <= in both so the chart will seem connected,
-    # otherwhise there will be a hole in the data
-    data_before = station_data_percentage[
-        (station_data_percentage["station"] == SELECTED_STATION) &
-        (station_data_percentage["pollutant"] == pollutant) &
-        (station_data_percentage["date"] <= pd.to_datetime(today))
-    ]
-    data_previs = station_data_percentage[
-        (station_data_percentage["station"] == SELECTED_STATION) &
-        (station_data_percentage["pollutant"] == pollutant) &
-        (station_data_percentage["date"] >= pd.to_datetime(today))
-    ]
-    
-    before = px.line(
-        data_before,
-        x = "date",
-        y = "value",
-        markers = True
+    Args:
+        station (str): the station to be displayed
+        pollutant (str): the pollutant to be displayed
+        time_span (str): values can be 'D': day, 'Y': year, 'H': hour
+
+    Returns:
+        plotly.graph_objs.Figure: the graph
+    """
+    fig = go.Figure()
+    today = dt.date.today()
+
+    past_data = get_percentage(
+        get_mean(
+            past,
+            time_span,
+            station,
+            pollutant
+        )
     )
 
-    previs = px.line(
-        data_previs,
-        x = "date",
-        y = "value",
-        markers = True,
-        line_dash_sequence = ["dash"]
+    prevision_data = get_percentage(
+        get_mean(
+            prevision,
+            time_span,
+            station,
+            pollutant
+        )
     )
 
-    fig = go.Figure(
-        data = before.data + previs.data
+    # past data
+    fig.add_trace(
+        go.Scatter(
+            x = past_data["date"],
+            y = past_data["value"],
+            mode = "lines+markers"
+        )
     )
+
+    # prevision_data
+    fig.add_trace(
+        go.Scatter(
+            x = prevision_data["date"],
+            y = prevision_data["value"],
+            mode = "lines+markers",
+            line = {"dash": "dash"}
+        )
+    )
+
     # horizontal maximum line
     fig.add_hline(
         y = 1,
@@ -85,21 +139,21 @@ def update_daily_graph(pollutant):
         line_color = "green",
         line_dash = "dash"
     )
+ 
     return fig
 
 ####################
 # SECTION 2 GRAPHS #
 ####################
 
-# TODO: add callback
-# VARIABLES NEEDED: verified_data -> appa's certified data
-#                   prevision_data -> FBK model's prediction 
+# VARIABLES NEEDED: VERIFIED_DATA  -> appa's certified data
+#                   PREVISION_DATA -> FBK model's prediction 
 def update_comparison_graph(
     station: str,
     pollutant: str,
     time_span: str,
     compare_APPA: bool
-):
+) -> plotly.graph_objs.Figure:
     """
     Updates the graph representing the comparison between
     APPA data and model prediction
@@ -109,15 +163,14 @@ def update_comparison_graph(
         pollutant (str): pollutant name
         time_span (str): H: hour; D: day; W: week; M: month; Y: year
         compare_APPA (bool): wether to add a line of APPA's data or not
+
+    Returns:
+        plotly.graph_objs.Figure: the graph
     """
-    compare_APPA = True
-    station = "Parco S. Chiara"
-    pollutant = "PM10"
-    time_span = "H"
     fig = go.Figure()
 
     prevision_data = get_mean(
-        prevision_data,
+        PREVISION_DATA,
         time_span,
         station,
         pollutant
@@ -133,7 +186,7 @@ def update_comparison_graph(
 
     if compare_APPA:
         appa_data = get_mean(
-            verified_data,
+            VERIFIED_DATA,
             time_span,
             station,
             pollutant
