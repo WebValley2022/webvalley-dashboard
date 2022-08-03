5a24b48f0d9c4ed7d4a39b4aca4c6702333165a7
Marco Di Francesco <marcodifran@gmail.com>
2022-08-03 13:24:49 +0000

    Add fbk raw data fetch from database

---
+++ plotly-app/db_utils.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import psycopg2
+from datetime import datetime
+
+def load_data_from_psql(query: str, columns: list[str] | None = None, **kwargs) -> pd.DataFrame:
+    # todo: reuse connection maybe?
+    with psycopg2.connect(
+        database="webvalley2022", user='postgres',
+        password='postgres',
+        host='localhost', port='5432'
+    ) as conn:
+        with conn.cursor() as curs:
+            curs.execute(query)
+            result = curs.fetchall()
+            if columns is None:
+                columns = [desc[0] for desc in curs.description]
+    # for some reason exiting the connection’s with block doesn’t close the connection automatically
+    conn.close()
+    return pd.DataFrame(result, columns=columns, **kwargs)
--- plotly-app/pages/fbk-raw.py
+++ plotly-app/pages/fbk-raw.py
@@ -5,8 +5,47 @@ import dash_bootstrap_components as dbc
 import pandas as pd
 import plotly.graph_objects as go
 from datetime import date
+import os
+from db_utils import load_data_from_psql
+from datetime import datetime
+import logging
 
 dash.register_page(__name__, redirect_from=["/"])
+global dfFBK1
+
+if os.getenv("DEBUG"):
+    dfFBK1 = pd.read_csv('FBK data/appa1_new.csv', encoding='windows-1252')
+else:
+    query = """
+    select
+        n.node_name as node_name,
+        n.description as node_description,
+        s.description as sensor_description,
+        p.sensor_ts as ts,
+        pd.r1 as signal_res,
+        pd.r2 as heater_res,
+        pd.volt as volt,
+        p.attrs::json->'G' as g,
+        p.attrs::json->'H' as h,
+        p.attrs::json->'P' as p,
+        p.attrs::json->'T' as t,
+        p.attrs::json->'RH' as rh,
+        p.attrs::json->'TH' as th,
+        p.attrs::json->'CFG' as cfg,
+        p.attrs::json->'CO2' as co2,
+        p.attrs::json->'IAQ' as iaq,
+        p.attrs::json->'VOC' as voc,
+        p.attrs::json->'IAC_comp' as iac_comp
+    from packet_data pd
+        left join packet p on p.id = pd.packet_id
+        left join sensor s on s.id = pd.sensor_id
+        left join node n on n.id = p.node_id
+    order by p.sensor_ts;
+    """
+    start = datetime.now()
+    df = load_data_from_psql(query)
+    logging.debug("Query time", datetime.now() - start)
+    df.to_csv("data_fbk_from_db.csv")
 
 title = html.Div("FBK Raw Data", className="header-title")
 
