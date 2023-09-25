c29e47b1bdbeffef219195bd5c5cda126fdf995f
Marco Di Francesco <marcodifran@gmail.com>
2023-09-25 16:09:03 +0000

    fix KerasWrapper also in fbk.py and utc timezone interval

--- plotly-app/pages/fbk.py
+++ plotly-app/pages/fbk.py
@@ -10,7 +10,51 @@ import numpy as np
 import dash
 import joblib
 from tensorflow.keras.models import load_model
+from keras import Sequential
+from sklearn.base import BaseEstimator, TransformerMixin
 from datetime import timedelta, datetime
+import pytz
+
+class KerasWrapper(BaseEstimator, TransformerMixin):
+    def __init__(self, num_epochs=100, verbose=0, **kwargs):
+        self.num_epochs = num_epochs
+        self.verbose = verbose
+        self.model = Sequential(**kwargs)
+        
+    def fit(self, X, y):
+        self.model.compile(loss='mean_absolute_error', optimizer='adam', metrics=['mean_squared_error'])
+        self.model.fit(X, y, validation_split = 0.2 ,epochs=self.num_epochs, verbose=self.verbose)
+        return self
+    
+    def transform(self, X):
+        # Perform any necessary transformations
+        return X
+    
+    def predict(self, X):
+        return self.model.predict(X)
+    
+    
+    def __getstate__(self):
+        # Get the state to be pickled
+        state = self.__dict__.copy()
+
+        # Remove the unpicklable model object
+        del state['model']
+
+        return state
+
+    def __setstate__(self, state):
+        # Restore the state from the unpickled state
+        self.__dict__.update(state)
+
+        if 'model_weights' in state:
+            self.model = Sequential()
+            self.model.set_weights(state['model_weights'])
+            self.model.compile(loss=state['model_loss'], optimizer=state['model_optimizer'], metrics=state['model_metrics'])
+        else:
+            self.model = None
+
+
 
 dash.register_page(__name__)
 
@@ -323,9 +367,18 @@ def update_comparison_graph(
     
     raw_data = get_data_day(start, end)
     
-    utc_now = datetime.utcnow()
-    asd = datetime.now() - utc_now
-    raw_data['ts'] = raw_data['ts'] + asd
+    italian_timezone = pytz.timezone('Europe/Rome')
+
+    # Get the current time in Italy
+    current_time_italy = datetime.now(italian_timezone)
+    current_time_italy = current_time_italy.replace(tzinfo=None)
+
+    # Get the current time in UTC
+    current_time_utc = datetime.now(pytz.UTC)
+    current_time_utc = current_time_utc.replace(tzinfo=None)
+    # Calculate the time difference
+    time_difference = current_time_italy - current_time_utc
+    raw_data['ts'] = raw_data['ts'] + time_difference
     test = raw_data.drop('ts',axis=1)
     
     y_pred=pipeline2.predict(test.values)
